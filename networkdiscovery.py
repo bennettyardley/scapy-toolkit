53ba4cf80783f6bb2fbcdfa4303dbc45299d3c29
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-06-22 18:52:32 -0400

    Update networkdiscovery.py

--- networkdiscovery.py
+++ networkdiscovery.py
@@ -4,9 +4,9 @@ import logging
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-addr_const = "192.168.1."
+addr = "192.168.1."
 
-for addr in range(1,255):
-    ans = sr1(IP(dst=addr_const+str(addr))/ICMP(), timeout=1, verbose=0)
-    if ans:
-        print ("Host: " + ans.src)
+for ending in range(1,255):
+    anwser = sr1(IP(dst=addr+str(ending))/ICMP(), timeout=1, verbose=0)
+    if anwser:
+        print ("Host: " + anwser.src)
