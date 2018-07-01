e8a4022bdbb3de54e2ce4874756ee4d345a62c3e
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-06-30 20:11:52 -0400

    Update pingscan.py

--- pingscan.py
+++ pingscan.py
@@ -6,7 +6,7 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 addr = "192.168.1."
 
-for  in range(1,255):
+for ending in range(1,255):
     anwser = sr1(IP(dst=addr+str(ending))/ICMP(), timeout=1, verbose=0)
     if anwser:
         print ("Host: " + anwser.src)
