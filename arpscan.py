a8063499d7a572af27e78c42fba7a211b2baad42
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-07-17 17:25:09 -0400

    Update arpscan.py

--- arpscan.py
+++ arpscan.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 iprange = "192.168.1.1/24"
 ans,unans=srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=iprange),
-              =2)
+              timeout=2)
 
 for snd,rcv in ans:
     print rcv.sprintf(r"%ARP.psrc%,%Ether.src%")
