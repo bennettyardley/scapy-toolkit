bfdf0435cb7adb40eda61e210a5d1cbca5562cc6
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-08-12 13:15:20 -0400

    Create traceroute.py

---
+++ traceroute.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+
+hostname = input("Enter hostname or IP for traceroute: ")
+
+for i in range(1, 30):
+    packet = IP(dst=hostname, ttl=i) / UDP(dport=33434)
+    response = sr1(packet, verbose=0)
+    if response is None:
+        break
+    elif response.type == 3:
+        print ("Final Hop " + response.src)
+        break
+    else:
+        print (str(i) + " " + response.src)
