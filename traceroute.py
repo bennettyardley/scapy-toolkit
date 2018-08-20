c97d2e3b997ac8720a3098104adf2215388564bb
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-08-19 22:43:27 -0400

    Update traceroute.py

--- traceroute.py
+++ traceroute.py
@@ -4,7 +4,7 @@ hostname = input("Enter hostname or IP for traceroute: ")
 
 for i in range(1, 30):
     packet = IP(dst=hostname, ttl=i) / UDP(dport=33434)
-     = sr1(packet, verbose=0)
+    response = sr1(packet, verbose=0)
     if response is None:
         break
     elif response.type == 3:
