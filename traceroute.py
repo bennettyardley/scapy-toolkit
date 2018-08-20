2849bde4ec0385e4594e242daa8d6b5c61214c6e
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-08-19 22:42:59 -0400

    Update traceroute.py

--- traceroute.py
+++ traceroute.py
@@ -2,7 +2,7 @@ from scapy.all import *
 
 hostname = input("Enter hostname or IP for traceroute: ")
 
-for i in range(1, 30):
+ i in range(1, 30):
     packet = IP(dst=hostname, ttl=i) / UDP(dport=33434)
     response = sr1(packet, verbose=0)
     if response is None:
